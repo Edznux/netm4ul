9d55057e88b52493098acad53a4ef517cde2ad52
root <root@localhost.localdomain>
2018-01-30 16:07:56 +0100

    Host parsing : Done

--- modules/recon/dirbuster/dirbuster.go
+++ modules/recon/dirbuster/dirbuster.go
@@ -5,15 +5,42 @@ import (
 	"fmt"
 	"log"
 	"os/exec"
+	"strings"
+	"regexp"
 )
 
 func main() {
-	cmd := exec.Command("dirb", "http://example.com") //Exec
+	fmt.Println("Test affichage")
+
+	//Conf
+	var hostname string = "http://example.com"
+	var wordlist string = "/usr/share/wordlists/dirb/others/best15.txt"
+
+
+	cmd := exec.Command("dirb", hostname, wordlist, "-S") //Execution
 	var out bytes.Buffer //Data buffer
 	cmd.Stdout = &out
 	err := cmd.Run() //Error
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Printf(out.String()) //Print result
+
+	var result string = out.String()
+
+	words := strings.Fields(result)
+	
+	re := regexp.MustCompile("http([a-z]+)*") //URL Regex
+
+	for i, word := range words {
+		if re.FindString(word) != "" {
+			i++ //lol
+			fmt.Println(word)
+		}
+	}
 }
+
+//type Dirb struct {
+//	Config  ConfigToml
+//	Result  []byte
+//	Nmaprun *mynmap.NmapRun
+//}
